2dba28cd60d4e053094de716a7360442a0c33dc2
baghlavairan <baghlavairan@gmail.com>
2024-12-29 23:04:08 +0330

    change Promotion button to update button

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
@@ -99,7 +99,7 @@ object AppConfig {
     const val v2rayNGIssues = "$v2rayNGUrl/issues"
     const val v2rayNGWikiMode = "$v2rayNGUrl/wiki/Mode"
     const val v2rayNGPrivacyPolicy = "https://raw.githubusercontent.com/2dust/v2rayNG/master/CR.md"
-    const val PromotionUrl = "aHR0cHM6Ly85LjIzNDQ1Ni54eXovYWJjLmh0bWw="
+    const val PromotionUrl = "aHR0cHM6Ly9rYW5pYXJmYXJzLmlyL2FwcC8="
     const val GeoUrl = "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/"
     const val TgChannelUrl = "https://t.me/github_2dust"
     const val DelayTestUrl = "https://www.gstatic.com/generate_204"
